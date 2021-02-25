994d056a9c1e0556452374623f876054497c8ec5
jamesqj <271686059@qq.com>
2021-02-25 14:14:55 +0800

    Create core5.03.sh

---
+++ core5.03.sh
@@ -0,0 +1,11 @@
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.asc.gpg
+mv microsoft.asc.gpg /etc/apt/trusted.gpg.d/
+wget -q https://packages.microsoft.com/config/debian/10/prod.list
+mv prod.list /etc/apt/sources.list.d/microsoft-prod.list
+chown root:root /etc/apt/trusted.gpg.d/microsoft.asc.gpg
+chown root:root /etc/apt/sources.list.d/microsoft-prod.list
+apt-get -y install apt-transport-https
+apt-get update
+apt-get -y install aspnetcore-runtime-5.0
+apt-get autoremove -y
+rm -rf /var/lib/apt/lists/*
